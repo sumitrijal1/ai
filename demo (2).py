e15d77b2aa94a8e238b7bbcea70638494ffc5b21
sumitrijal1 <rijalsumit50@gmail.com>
2026-01-22 09:42:45 +0545

    changes

--- demo (2).py
+++ demo (2).py
@@ -4,7 +4,7 @@ import joblib
 model = joblib.load('knn_model.joblib')
 st.title("Books Classifier")
 
-Num_Pages = st.number_input(label="Num Pages",min_value=0.0,max_value=10.0,value=5.0)
+Num_Pages = st.number_input(label="Num Pages",min_value=0.0,max_value=1000.0,value=5.0)
 Avg_WordLength= st.number_input(label="Avg WordLength",min_value=0.0,max_value=10.0,value=3.5)
 Reading_TimeHours = st.number_input(label="Reading TimeHours",min_value=0.0,max_value=10.0,value=1.5)
 Complexity_Score = st.number_input(label="Complexity Score",min_value=0.0,max_value=10.0,value=0.2)
